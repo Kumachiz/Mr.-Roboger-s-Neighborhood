e0ca6700344e1f5fc99cb8bdc63bfbcfaf0434e4
Michael Munyaneza <kuamchiz@gmail.com>
2021-11-21 01:57:40 -0800

    add the main commit with the project layout and some functioning bits

---
+++ js/scripts.js
@@ -0,0 +1,53 @@
+//Business Logic
+
+function beepBoop(number) {
+	let result = [];
+
+	for (let i=0; i<=number; i++) {
+		const numbers = getNumbers(i);
+
+		if (numbers.includes(3)) {
+			result.push("Won't you be my neighbor?");
+		}
+		else if (numbers.includes(2)) {
+			result.push("Boop!");
+		}
+		else if (numbers.includes(1)) {
+			result.push("Beep!");
+		}
+		else {
+			result.push(i);
+		}
+	}
+
+	return result;
+}
+
+
+function getNumbers(number) {
+	let numbers = [];
+	for (let currentNumber = number; number >= 1; number = Math.floor(number / 10)) {
+		numbers.push(number % 10)
+	}
+
+	return numbers;
+}
+
+//UI Logic
+
+$(document).ready(function() {
+	$("#inputForm").submit(function(event) {
+		event.preventDefault();
+		
+		$("#results").show();
+		$("ul#numberList").empty();
+
+		inputNumber = parseInt($("input#numberInput").val());
+    $("#inputForm")[0].reset();
+		
+		const results = beepBoop(inputNumber);
+		for (const result of results) {
+			$("ul#numberList").append("<li>" + result + "</li>");
+		}
+	})
+});
